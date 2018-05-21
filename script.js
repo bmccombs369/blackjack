356d358f3c3d07f57165554a5726258ce935075e
bmccombs369 <bmccombs369@gmail.com>
2018-05-21 18:41:15 -0400

    linked stylesheets

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const Deck = {
     face: 'ace',
     suit: 'diamonds',
     value: 11
-    },
+},
     {
-        face:'ace',
-        suit:'hearts',
-        value:11
-    },
+        face: 'ace',
+        suit: 'hearts',
+        value: 11
+},
     {
-        face:'ace',
-        suit:'spades',
-        value:11
-    },
+        face: 'ace',
+        suit: 'spades',
+        value: 11
+},
     {
-        face:'ace',
-        suit:'spades',
-        value:11
-    },
+        face: 'ace',
+        suit: 'spades',
+        value: 11
+},
+    {
+        face: 'ace',
+        suit: 'spades',
+        value: 11
+},
 };
