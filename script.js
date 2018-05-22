afb387c3f00d1d5a916e09d92e234d3caaa25e85
bmccombs369 <bmccombs369@gmail.com>
2018-05-22 19:14:42 -0400

    add jquery code for stay button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'K
 const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
 const cards = [];
 
-// creates a 52 card deck on page loading
+//**creates a 52 card deck on page loading
 function createDeck() {
   for (var i = 0; i < suits.length; i++) {
     for (var j = 0; j < faces.length; j++) {
@@ -21,7 +21,7 @@ let dealersHand = [];
 const randomNumber = function () {
   return Math.floor(Math.random() * cards.length);
 }
-// deals 2 cards to player and dealer
+//**deals 2 cards to player and dealer
 function deal() {
   for (i = 0; i < 2; i++) {
     if (playersHand.length < 2) {
@@ -35,7 +35,7 @@ function deal() {
   console.log(dealersHand);
 }
 
-// hits player with one card
+//**hits player with one card
 function hitPlayer() {
   playersHand.push(cards.splice(randomNumber(), 1)[0]);
   console.log(playersHand);
@@ -44,7 +44,7 @@ function hitPlayer() {
   // }
 }
 
-//player stays and passes turn to the dealer
+//**player stays and passes turn to the dealer
 function stay() {
   // if (/*dealersTotal*/ <= 16) {
   //   dealersHand.push(cards.splice(randomNumber(), 1)[0]);
@@ -54,22 +54,25 @@ function stay() {
   // }
 }
 
-//reset the game
+//**reset the game
 function reset() {
   let playersHand = [];
   let dealersHand = [];
   //add deal function call to turn into a redeal button
 }
 
-// jquery for buttons
+//**jquery for buttons
 $('.deal').click(function () {
-  deal()
+  deal();
 }
 );
 $('.hit').click(function () {
-  hitPlayer()
+  hitPlayer();
 }
 );
+$('.stay').click(function () {
+  stay();
+})
 
 const playerCardValue1 = playersHand[0].value;
 const playerCardValue2 = playersHand[1].value;
