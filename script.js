d69060faf9cb97b629d678b3207fa53536bec80f
bmccombs369 <bmccombs369@gmail.com>
2018-05-22 14:59:18 -0400

    started deal function/still needs a way to pull dealt cards out of cards array

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+const suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds'];
+const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
+const cards = [];
 
 // creates a 52 card deck
 function createDeck() {
-  const suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds'];
-  const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
-  const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
-  const cards = [];
   for (var i = 0; i < suits.length; i++) {
     for (var j = 0; j < faces.length; j++) {
       var card = { suit: suits[i], face: faces[j], value: values[j] }
@@ -13,15 +13,27 @@ function createDeck() {
   }
   console.log(cards);
 }
+  let playersHand = [];
+  let dealersHand = [];
+  function deal () {
+    for (i = 0; i < 2; i++) {
+      if (playersHand.length < 2) {
+        playersHand.push(cards[Math.floor(Math.random() * cards.length)]);
+        console.log(playersHand);
+      }
+      if (dealersHand.length < 2) {
+        dealersHand.push(cards[Math.floor(Math.random() * cards.length)]);
+        console.log(dealersHand);
+      }
+    }
+  }
 
-const deal = {
-  //creates a random number for dealing
-  randomNumber: function () {
-    const random = Math.floor(Math.random() * 53);
-    return random;
-  },
-  dealCards: function () {
-    const card1 = deck.createDeck().cards[this.randomNumber()];
-    return card1;
-  },
-};
+
+// function randomNumber() {
+//   card = Math.floor(Math.random() * 52);
+//   return card;
+// }
+// const card1 = cards[randomNumber()];
+// console.log(card1)
+// const card2 = cards[randomNumber()];
+
