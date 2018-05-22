001c32f816a72174527471cc56f2f65d3105898e
bmccombs369 <bmccombs369@gmail.com>
2018-05-22 10:54:27 -0400

    started creating Card class

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-const Deck = {
-    face: 'ace',
-    suit: 'diamonds',
-    value: 11
-},
-    {
-        face: 'ace',
-        suit: 'hearts',
-        value: 11
-},
-    {
-        face: 'ace',
-        suit: 'spades',
-        value: 11
-},
-    {
-        face: 'ace',
-        suit: 'spades',
-        value: 11
-},
-    {
-        face: 'ace',
-        suit: 'spades',
-        value: 11
-},
+function createDeck() {
+  const suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds'];
+  const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+  const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
+  const deck = [];
+  for (var i = 0; i < this.suits.length; i++) {
+    for (var j = 0; j < this.faces.length; j++) {
+      this.deck.push(this.suits[i], this.faces[j], this.values[j]);
+    }
+  }
+  console.log(this.deck);
 };
+createDeck();
+console.log(deck.length);
